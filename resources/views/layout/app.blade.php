cefb8c69512798b74f5d7b86d04645749a727fd7
Aditya Raj <aditya.rajb@gmail.com>
2025-08-02 15:08:36 +0530

    flv test vercel

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -10,9 +10,12 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=inter:400,500,600,700&family=poppins:400,500,600,700" rel="stylesheet" />
 
-        <!-- Styles / Scripts -->
-        @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
-            @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @if(app()->environment('production'))
+            <link rel="stylesheet" href="{{ asset('build/app.css') }}">
+        @else
+            @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+                @vite(['resources/css/app.css', 'resources/js/app.js'])
+            @endif
         @endif
     </head>
     <body class="font-sans antialiased bg-gradient-to-br from-slate-50 to-blue-50 min-h-screen">
